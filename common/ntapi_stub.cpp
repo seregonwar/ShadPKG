b52388dbfc5f795771ee405aa92cd64d6294ab6a
Anonymous Maarten <anonymous.maarten@gmail.com>
2026-01-10 01:51:49 +0100

    NtSetInformationFile is Windows-only

--- common/ntapi_stub.cpp
+++ common/ntapi_stub.cpp
@@ -2,8 +2,10 @@
 #include "common/ntapi.h"
 #include "common/shadpkg_types.h"
 
+#ifdef _WIN32
 static u64 NtSetInformationFile_stub(HANDLE, PIO_STATUS_BLOCK, PVOID, ULONG, FILE_INFORMATION_CLASS) {
     return 0;
 }
 
-NtSetInformationFile_t NtSetInformationFile = NtSetInformationFile_stub; 
+NtSetInformationFile_t NtSetInformationFile = NtSetInformationFile_stub;
+#endif
